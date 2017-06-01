1e956cf825ce1f87988dcf9322f2cb663a2017f4
belen95 <belen_95_22@hotmail.es>
2017-06-01 15:44:55 +0200

    Update PrintDot.java

--- Vlcl/src/interp/PrintDot.java
+++ Vlcl/src/interp/PrintDot.java
@@ -1,25 +1,40 @@
 package interp;
 
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
 
 public class PrintDot {
 
     private String dot;
+    private int num;
     
     public PrintDot() {
         dot = "";
+        num = 1;
+    }
+    
+    public String getDot() {
+        return dot;
+    }
+    
+    public void moduleHead() {
+        dot += "digraph ";
+        dot += num;
+        ++num;
+        dot += " {\n"
     }
 
-    public String printNode(String name, DataNode node) {
-        dot += "name -> {";
+    public void newNode(String name, DataNode node) {
+        dot += name + " -> {";
         List<SignalItem> outputs = node.getOutputs();
         for(int i = 0; i < ouputs.size(); ++i) {
             dot += outputs[i].getName();
+            dot += " ";
         }
         dot += "}\n";
+    }
+    
+    public void moduleFooter() {
+        dot += "}\n\n";
     }
 
 }
