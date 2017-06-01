ae23fea0c8bac6316a0ed1f3cf6cf38d810dc461
belen95 <belen_95_22@hotmail.es>
2017-06-01 16:40:19 +0200

    Update ReadFile.java

--- Vlcl/src/interp/ReadFile.java
+++ Vlcl/src/interp/ReadFile.java
@@ -1,42 +1,47 @@
 package interp;
 
+import java.util.Map;
 import java.io.File;
 import java.util.Scanner;
 
 public class ReadFile {
 
-    public static void main(String[] args) {
+    private Map<String,Coords> nodes;
 
+    public void getNodeCoordsFromTex(String texFile) {
         try {
+        Scanner input = new Scanner(texFile);
 
-            File file = new File(args[0]);
+        String line;
 
-            Scanner input = new Scanner(file);
-
-            String line;
-
-            while (input.hasNextLine()) {
-                line = input.nextLine();
-                if (line.length() > 10) {
-                    if (line.substring(0,10).equals("  % Node: ")) {
-                        System.out.println(line.substring(10,line.length()));
-                        for (int i = 0; i < 4; ++i)
-                            line = input.nextLine();
-                        int i = 9;
-                        while (line.charAt(i) != 'b') ++i;
-                        System.out.println(line.substring(9,i));
-                        i += 3;
-                        int j = i;
-                        while (line.charAt(i) != 'b') ++i;
-                        System.out.println(line.substring(j,i));
-                    }
-        		}
+        while (input.hasNextLine()) {
+            line = input.nextLine();
+            if (line.length() > 10) {
+                if (line.substring(0,10).equals("  % Node: ")) {
+                    String nodeName = line.substring(10,line.length());
+                    Coords c = new Coords();
+                    for (int i = 0; i < 4; ++i)
+                        line = input.nextLine();
+                    int i = 9;
+                    while (line.charAt(i) != 'b') ++i;
+                    c.setFirstCoord(Integer.parseInt(line.substring(9,i)));
+                    i += 3;
+                    int j = i;
+                    while (line.charAt(i) != 'b') ++i;
+                    c.setSecCoord(Integer.parseInt(line.substring(j,i)));
+                    nodes.put(nodeName,c);
+                }
             }
-            input.close();
+        }
+        input.close();
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+    
+    public Coords getCoordsFromNode(String nodeName) {
+        return nodes.get(nodeName);
     }
 
 }
